65b814eec1039cec74010298b8ffc07d8db6f8b8
Bhagyashree Sonawane <shreesonawane35@gmail.com>
2024-12-27 05:59:25 +0000

    saving the variable code

---
+++ Basics/variable.js
@@ -0,0 +1,10 @@
+const accountId = 1234;
+let accountEmail = "Shree@gmail.com";
+let accountPassword = 4567;
+let accountCity = "Nashik";
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+//Prefer not used var because od block issue and functional scope
