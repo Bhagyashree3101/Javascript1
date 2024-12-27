439b7020f660a94c9f94cf12c566aa1889557aaf
Bhagyashree Sonawane <shreesonawane35@gmail.com>
2024-12-27 06:30:41 +0000

    datatpes and conversion

---
+++ Basics/coversion.js
@@ -0,0 +1,2 @@
+let score = 32;
+console.log(typeof(score));
---
+++ Basics/datatypes.js
@@ -0,0 +1,15 @@
+let name = "shree";
+let age = 21;
+let isLoggedIn = true;
+
+console.log(name,age);
+console.log(isLoggedIn);
+console.log(typeof "Shree");
+
+//number => 2 to power of 6
+// string => " " should be write in double quotes
+//boolean => true/false
+//null => standalone value/empty value i.e a=null
+//undefineed => variable declared but not value
+//symbol => unique used in react
+//object
